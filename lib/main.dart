6414420597135a9da8f04fcb24ab0479d8216f98
Samuel <samuelalves681@gmail>
2022-12-27 23:09:47 -0300

    remove ununsed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'dart:ffi';
 
 import 'package:alcool_gasolina/pages/home.page.dart';
-import 'package:alcool_gasolina/widgets/input.widget.dart';
-import 'package:alcool_gasolina/widgets/load-button.widget.dart';
-import 'package:alcool_gasolina/widgets/logo.widget.dart';
-import 'package:alcool_gasolina/widgets/submit-form.widget.dart';
-import 'package:alcool_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
   runApp(const MyApp());
