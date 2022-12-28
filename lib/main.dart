48e86691d53f61bf51952618c17d60d282c548ba
Samuel <samuelalves681@gmail>
2022-12-27 22:08:50 -0300

    widget success

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:alcool_gasolina/widgets/input.widget.dart';
 import 'package:alcool_gasolina/widgets/load-button.widget.dart';
 import 'package:alcool_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_gasolina/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -38,31 +39,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).primaryColor,
       body: ListView(children: <Widget>[
         Logo(),
-        Container(
-          margin: EdgeInsets.all(30),
-          decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25)),
-          child: Column(children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            Text('Compensa utilizar álcool',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: 'Big Shoulders Display')),
-            SizedBox(
-              height: 20,
-            ),
-            LoadingButton(
-                busy: false,
-                invert: true,
-                func: () {},
-                text: 'Calcular Novamente')
-          ]),
-        ),
+        Success(reset: () {}, result: 'Compensa utilizar X'),
         Input(label: 'Gasolina', ctrl: _gasCtrl),
         Input(label: 'Álcool', ctrl: _alcCtrl),
         LoadingButton(busy: true, invert: false, func: () {}, text: 'Calcular')
